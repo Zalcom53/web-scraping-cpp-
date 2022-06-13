888d5e5e8996f0e5cbf0a384db602a736fee630d
Antonio Quihuis <zalcom53@gmail.com>
2022-06-13 10:30:57 -0700

    .
    .

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+  Nombre: main.cpp
+  Autor: Daniel Antonio Quihuis Hernandez
+  Fecha: año 2022
+  Descripcion: Este archivo contiene la implementacion del archivo utileria.h
+*/
+
+##include <utileria.h>
+
+
+/** \fn int main()
+ * \brief realiza la implementacion del archivo utileria.cpp
+ * \param no recibe parametros
+ * \return Devuelve un 0
+*/
+int main(){
+
+run();
+
+cout << "Presione una tecla para continuar...\n" << endl;
+cin.get();
+} // LLave de cierre en el
---
+++ utileria.cpp
@@ -0,0 +1,42 @@
+/*
+  Nombre: utileria.h
+  Autor: Daniel Antonio Quihuis Hernandez
+  Fecha: año 2022
+  Descripcion:
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+//----------------------------------------------------
+// BLOQUE PARA DECLARAR FUNCIONES
+//----------------------------------------------------
+
+
+
+
+/** \fn run()
+ * \brief ejecuta todo el codigo implementado en una sola funcionn
+ * \param no recibe parametros
+ * \return no retorna valores de ningun tipo 
+*/
+void run(){
+cout << "web-scraping c++ | Pruebas\n" << endl;
+
+} // LLave de cierre en la funcion run
+
+void obtenerArchivo(){
+
+cout << "Obteniendo archivo de texto espere un momento...\n" << endl;
+
+}// Llave de cierre en la funcion obtener archivo
+
+
+void procesarArchivo(){
+
+  cout << "Procesando archivo espere un momenot...\n" << endl;
+
+
+}// Llave de cierre en la funcion procesarArchivo
